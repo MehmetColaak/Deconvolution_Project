4612415c9d0501ab39d3d71966464e55d8d9b766
MehmetColaak <colak.mehmet@hotmail.com>
2024-04-13 15:54:09 +0200

    Wavefile Plotting feature added

--- Deconvolution.py
+++ Deconvolution.py
@@ -4,6 +4,7 @@ import librosa as rosa
 import numpy as np
 import soundfile as sf
 from scipy.signal import fftconvolve
+import matplotlib.pyplot as plt
 
 def main():
     
@@ -13,12 +14,13 @@ def main():
 
     #Terminal print texts for user input
     print("Please select your reference file:")
-    reference_path = filedialog.askopenfilename()
+    reference_path = filedialog.askopenfilename(title="Reference Wave File")
     print("Please select your recorded audio file:")
-    recorded_path = filedialog.askopenfilename()
+    recorded_path = filedialog.askopenfilename(title="Recorded Wave File")
 
     if reference_path and recorded_path:
         signalProcess(reference_path, recorded_path)
+        plotFig()
     else:
         print("File selection cancelled.")
 
@@ -29,15 +31,26 @@ def signalProcess(reference_path, recorded_path):
     rec_signal, _ = rosa.load(recorded_path, sr=sr, mono=True)
 
     #Deconvolution signal processing
+    global impulse_response
     impulse_response = fftconvolve(rec_signal, ref_signal[::-1], mode='full')
     impulse_response /= np.abs(impulse_response).max()
     impulse_response = impulse_response[len(ref_signal):]
 
     #File saveas dialog
-    file_path = filedialog.asksaveasfilename()
+    global file_path
+    file_path = filedialog.asksaveasfilename(defaultextension=".wav", title="Save Impulse Response as Wave File")
 
     print("Your IR is ready at: " + file_path)
-    sf.write(file_path + ".wav", impulse_response, sr)
+    sf.write(file_path, impulse_response, sr)
+
+def plotFig():
+    plt.figure(figsize=(20, 5))
+    plt.title("Impulse Response Wavefile Figure")
+    plt.xlabel("Sample")
+    plt.ylabel("Amplitude")
+    plt.plot(impulse_response)
+    modified_filepath = file_path[:-4]
+    plt.savefig(modified_filepath + ".png")
 
 if __name__ == "__main__":
     main()
